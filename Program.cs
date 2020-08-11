df15d9b511f96ff4f6fd6d4285a46d0d19b80833
Mohamed Ammar <mohamed.ammar86@gmail.com>
2020-08-11 13:01:15 -0400

    first part of Homework

--- Program.cs
+++ Program.cs
@@ -2,11 +2,15 @@
 
 namespace VariablesCS
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            int numberOfCupsOfCoffee = 3;
+            string fullName = "Mohamed Ammar";
+            string today = "August 11th";
+
+            Console.WriteLine($"Hello, My name is {fullName}. I usually drink around {numberOfCupsOfCoffee} cups of coffee a day. Ohh I forgot to tell you that today's date is {today}.");
+        }
     }
-  }
 }
